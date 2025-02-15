12c5b062fef6c4ea2818f326762d92ac3ea02c74
xu (Tom) weikai <xu000306@algonquinlive.com>
2025-02-15 14:15:54 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-require('dotenv/config');
-const express = require('express');
+require("dotenv/config");
+const express = require("express");
 
 const { STUDENT_NAME } = process.env;
 
 const app = express();
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   if (!STUDENT_NAME) {
     res.status(500).json({
-      error: { message: 'Environment variable STUDENT_NAME not provided' }
+      error: { message: "Environment variable STUDENT_NAME not provided" },
     });
     return;
   }
   res.json({
-    data: `Hello from ${STUDENT_NAME}!`
+    data: `Hello from ${STUDENT_NAME}!`,
   });
 });
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log('App is listening!');
+  console.log("App is listening!");
 });
